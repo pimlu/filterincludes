838ad003827a7e16f7f513638e7bda8d08bc8fcd
Stuart Geipel <lapimlu@gmail.com>
2019-05-10 22:30:08 -0700

    format whitespace

--- src/extension.ts
+++ src/extension.ts
@@ -2,16 +2,16 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as os from 'os';
-import {dirname} from 'path';
+import { dirname } from 'path';
 import * as fs from 'fs';
-import {execFile} from 'child_process';
+import { execFile } from 'child_process';
 
 
-let info = (s:string) => vscode.window.showInformationMessage(s);
-let error = (s:string) => vscode.window.showErrorMessage(s);
-function runCommand(cmd: string, cwd:string) {
+let info = (s: string) => vscode.window.showInformationMessage(s);
+let error = (s: string) => vscode.window.showErrorMessage(s);
+function runCommand(cmd: string, cwd: string) {
   return new Promise((resolve, reject) => {
-    execFile('/bin/sh', ['-c', cmd], {cwd}, (e, out,err) => {
+    execFile('/bin/sh', ['-c', cmd], { cwd }, (e, out, err) => {
       //console.log(out.trim().split('\n').pop());
       //console.log(err.trim().split('\n').pop());
     }).on('exit', code => {
@@ -25,25 +25,25 @@ function getCwd(uri: vscode.Uri): string {
 
   const isFileOrUntitledDocument = uri && (uri.scheme === 'file' || uri.scheme === 'untitled');
   if (isFileOrUntitledDocument) {
-      const useDocumentDirAsWorkDir = vscode.workspace.getConfiguration('filterText').useDocumentDirAsWorkDir;
+    const useDocumentDirAsWorkDir = vscode.workspace.getConfiguration('filterText').useDocumentDirAsWorkDir;
 
-      if (useDocumentDirAsWorkDir && uri.scheme === 'file') {
-          return dirname(uri.fsPath);
-      }
+    if (useDocumentDirAsWorkDir && uri.scheme === 'file') {
+      return dirname(uri.fsPath);
+    }
 
-      const folder = vscode.workspace.getWorkspaceFolder(uri);
-      if (folder) {
-          return folder.uri.fsPath;
-      }
+    const folder = vscode.workspace.getWorkspaceFolder(uri);
+    if (folder) {
+      return folder.uri.fsPath;
+    }
 
-      const folders = vscode.workspace.workspaceFolders;
-      if (folders != undefined && folders.length > 0) {
-          return folders[0].uri.fsPath;
-      }
-      // Github #9: if no workspace folders, and uri.scheme !== 'untitled' (i.e. existing file), use folder of that file. Otherwise, use user home directory.
-      if (uri.scheme !== 'untitled') {
-          return dirname(uri.fsPath);
-      }
+    const folders = vscode.workspace.workspaceFolders;
+    if (folders != undefined && folders.length > 0) {
+      return folders[0].uri.fsPath;
+    }
+    // Github #9: if no workspace folders, and uri.scheme !== 'untitled' (i.e. existing file), use folder of that file. Otherwise, use user home directory.
+    if (uri.scheme !== 'untitled') {
+      return dirname(uri.fsPath);
+    }
   }
 
   return os.homedir();
@@ -53,11 +53,11 @@ function getCwd(uri: vscode.Uri): string {
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.filterIncludes', () => {
-		// The code you place here will be executed every time your command is executed
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand('extension.filterIncludes', () => {
+    // The code you place here will be executed every time your command is executed
 
 
     const editor = vscode.window.activeTextEditor;
@@ -65,7 +65,7 @@ export function activate(context: vscode.ExtensionContext) {
     if (editor.selections.length > 1) return error("Multiple text is selected!");
     let s = editor.selection;
     if (!s.start.isBefore(s.end)) return error("Range is not selected");
-    
+
     let options: vscode.InputBoxOptions = {
       prompt: "Command to run build",
       placeHolder: "cd build && make -j12",
@@ -83,8 +83,8 @@ export function activate(context: vscode.ExtensionContext) {
       let data;
       try {
         data = fs.readFileSync(filepath, 'utf8');
-      } catch(e) {}
-      if(!data || data.substring(lo, hi) !== linesText) 
+      } catch (e) { }
+      if (!data || data.substring(lo, hi) !== linesText)
         throw new Error("Filesystem error");
       let pre = data.substring(0, lo);
       let post = data.substring(hi, data.length);
@@ -101,21 +101,21 @@ export function activate(context: vscode.ExtensionContext) {
         // trick build systems into thinking this file is out of date
         // even though we're super fast (sometimes builds happen in <1s,
         // which is the timestamp resolution
-        let touchCmd = "touch -d '+1 minute' '"+filepath+"'";
-        if (touch && !await runCommand(touchCmd, cwd)) 
+        let touchCmd = "touch -d '+1 minute' '" + filepath + "'";
+        if (touch && !await runCommand(touchCmd, cwd))
           throw new Error('touch failed to set file timestamp');
       };
       let cwd = getCwd(editor.document.uri);
       if (!await runCommand(cmd, cwd))
-        throw new Error ("Initial invocation failed!");
-      for(let i=0; i<lines.length; i++) {
+        throw new Error("Initial invocation failed!");
+      for (let i = 0; i < lines.length; i++) {
         if (!/\S/.test(lines[i])) continue;
         keep[i] = false;
         await update(true);
         if (!await runCommand(cmd, cwd)) keep[i] = true;
       }
       await update(true);
-      if(!await runCommand(cmd, cwd)) {
+      if (!await runCommand(cmd, cwd)) {
         keep.fill(true);
         await update(false);
         throw new Error("Something went wrong, reverted.");
@@ -123,12 +123,12 @@ export function activate(context: vscode.ExtensionContext) {
       await update(false);
     }).then(() => info('Done!'), err => {
       if (err === 'blank') return;
-      error("Failure: "+err.message);
+      error("Failure: " + err.message);
     });
-	});
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
